1bc53d316e1170df06c6a62803888ca8ab4464cd
Yuki Kato <katoyuki0113@gmail.com>
2021-10-14 20:48:14 +0200

    Solved lab without styling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express");
+const router = new express.Router();
+const movieModel = require("./../models/movie.model")
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
 
+//const config = require("./../config/index");
+
+const homeController = (req, res, next) => {
+  res.render("index.hbs", {});
+};
+
+const movieController = ((req, res, next) => {
+  // avec le model
+  // fetch tous les documents movie
+  // donne les à la view
+  // dans la view {{#each}}
+   const movies = movieModel.find()
+    .then((movies) => res.render("movies.hbs", {movies})
+    .catch((err) => res.send("error"))
+
+)});
+
+const oneMovieController = async (req, res) => {
+    const movie = await movieModel.findById(req.params.id);
+    res.render("movieDetails.hbs", { movie });
+};
+
+
+router.get("/", homeController);
+router.get("/movies", movieController);
+router.get("/movies/:id", oneMovieController)
+// router.get("/all-cats/:id", oneCatController);
 module.exports = router;
